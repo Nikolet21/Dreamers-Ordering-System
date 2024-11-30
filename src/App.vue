75190bf77c32626bbea5aa5d778dc0af14705b7c
John Cez Casupanan <casupananjohncez@gmail.com>
2024-12-01 02:42:51 +0800

    Adds global styles
    Adds global styles using Poppins font and resets margin and padding for better consistency.

--- src/App.vue
+++ src/App.vue
@@ -14,3 +14,22 @@ onMounted(() => {
   userStore.initializeFromStorage()
 })
 </script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap');
+
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Poppins', sans-serif;
+}
+
+#app {
+  font-family: 'Poppins', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+/* Add any other global styles here */
+</style>
